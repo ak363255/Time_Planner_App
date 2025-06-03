c0826d1bb3d408d583de7cc58dc508cc860d2f64
Amit <ak363255@gmail.com>
2025-06-03 11:50:27 +0530

    App Base Classes added

--- app/src/main/java/com/example/timeplannerapp/MainActivity.kt
+++ app/src/main/java/com/example/timeplannerapp/MainActivity.kt
@@ -1,32 +1,79 @@
 package com.example.timeplannerapp
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.presentation.ui.views.SystemBarColor
+import com.example.timeplannerapp.presentation.ui.main.contract.DeepLinkTarget
+import com.example.timeplannerapp.presentation.ui.main.contract.MainDeps
+import com.example.timeplannerapp.presentation.ui.main.contract.MainEffect
+import com.example.timeplannerapp.presentation.ui.main.contract.MainEvent
+import com.example.timeplannerapp.presentation.ui.main.contract.MainViewState
+import com.example.timeplannerapp.presentation.ui.main.viewmodel.MainEffectCommunicator
+import com.example.timeplannerapp.presentation.ui.main.viewmodel.MainStateCommunicator
+import com.example.timeplannerapp.presentation.ui.main.viewmodel.MainViewmodel
 import com.example.timeplannerapp.presentation.ui.theme.TimePlannerAppTheme
+import com.example.utils.manager.CoroutineManager
 import com.example.utils.platform.screen.ScreenContent
+import com.example.utils.platform.screen.ScreenScope
+import com.example.utils.platform.screenmodel.contract.BaseViewState
 
 class MainActivity : ComponentActivity() {
+    val mainViewmodelFactory = MainViewmodel.MainViewModelFactory(
+        mainStateCommunicator = MainStateCommunicator.Base(),
+        mainEffectCommunicator = MainEffectCommunicator.Base(),
+        coroutineManager = CoroutineManager.Base()
+    )
+    val mainViewmodel: MainViewmodel by lazy {
+        ViewModelProvider(this,mainViewmodelFactory)[MainViewmodel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            ScreenContent(
+                screenModel = mainViewmodel,
+                initialState = MainViewState(),
+                dependencies = MainDeps(screenTarget = DeepLinkTarget.byIntent(intent))
+            ) {mainViewState ->
+                TimePlannerAppTheme{
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "splash"){
+                        composable(route = "splash") {
+                              Splash(mainViewState)
+                        }
+                        composable(route = "main") {
 
+                        }
+                    }
+                    SystemBarColor(
+                        statusBarColor = Color(0xFF6200EE),     // Purple
+                        navigationBarColor = Color(0xFF121212), // Dark
+                        isDarkIcon = true                    // Light icons
+                    )
+                }
 
-            TimePlannerAppTheme{
-                SystemBarColor(
-                    statusBarColor = Color(0xFF6200EE),     // Purple
-                    navigationBarColor = Color(0xFF121212), // Dark
-                    isDarkIcon = true                    // Light icons
-                )
             }
+
         }
     }
+
+    @Composable
+    fun  ScreenScope<MainViewState, MainEvent, MainEffect, MainDeps>.Splash(mainViewState: MainViewState){
+
+    }
+
+
 }
 
--- app/src/main/java/com/example/timeplannerapp/presentation/ui/main/viewmodel/MainViewmodel.kt
+++ app/src/main/java/com/example/timeplannerapp/presentation/ui/main/viewmodel/MainViewmodel.kt
@@ -1,41 +1,42 @@
 package com.example.timeplannerapp.presentation.ui.main.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.timeplannerapp.presentation.ui.main.contract.MainAction
 import com.example.timeplannerapp.presentation.ui.main.contract.MainDeps
 import com.example.timeplannerapp.presentation.ui.main.contract.MainEffect
 import com.example.timeplannerapp.presentation.ui.main.contract.MainEvent
 import com.example.timeplannerapp.presentation.ui.main.contract.MainViewState
 import com.example.utils.manager.CoroutineManager
-import com.example.utils.platform.communications.state.EffectCommunicator
 import com.example.utils.platform.screenmodel.BaseViewModel
 import com.example.utils.platform.screenmodel.work.BackgroundWorkKey
 import com.example.utils.platform.screenmodel.work.WorkScope
 
 class MainViewmodel(
-     mainStateCommunicator: MainStateCommunicator,
-     mainEffectCommunicator: MainEffectCommunicator,
+    mainStateCommunicator: MainStateCommunicator,
+    mainEffectCommunicator: MainEffectCommunicator,
     coroutineManager: CoroutineManager
-
 ) : BaseViewModel<MainViewState, MainEvent, MainAction, MainEffect, MainDeps>(
     stateCommunicator = mainStateCommunicator,
     effectCommunicator = mainEffectCommunicator,
     coroutineManager = coroutineManager
-){
+) {
     override suspend fun reduce(
         action: MainAction,
         currentState: MainViewState
     ): MainViewState {
-         return currentState // handle this
+        return currentState // handle this
     }
 
     override suspend fun WorkScope<MainViewState, MainAction, MainEffect>.handleEvent(
         event: MainEvent
     ) {
-        when(event){
+        when (event) {
             is MainEvent.Init -> {
-                launchBackgroundWork(key = BackgroundKey.LOAD_SETTING) {  }
-
+                launchBackgroundWork(key = BackgroundKey.LOAD_SETTING) { }
+                sendEffect(MainEffect.NavigateToMain)
             }
+
             MainEvent.NavigateToEditor -> {
 
             }
@@ -44,13 +45,28 @@ class MainViewmodel(
     }
 
     override fun init(deps: MainDeps) {
-        if(!isInitialize.get()){
+        if (!isInitialize.get()) {
             super.init(deps)
             dispatchEvent(MainEvent.Init(deps.screenTarget))
         }
     }
 
-    enum class BackgroundKey : BackgroundWorkKey{
-        LOAD_SETTING,NAVIGATE
+    enum class BackgroundKey : BackgroundWorkKey {
+        LOAD_SETTING, NAVIGATE
+    }
+
+    class MainViewModelFactory(
+        private val mainStateCommunicator: MainStateCommunicator,
+        private val mainEffectCommunicator: MainEffectCommunicator,
+        private val coroutineManager: CoroutineManager
+
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewmodel(
+                mainStateCommunicator = mainStateCommunicator,
+                mainEffectCommunicator = mainEffectCommunicator,
+                coroutineManager = coroutineManager
+            ) as T
+        }
     }
 }
